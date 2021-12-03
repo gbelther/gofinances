ff7c758400bb00b7af61379374a15b92dd58e900
GBelther <belthergabriel@gmail.com>
2021-12-02 23:20:23 -0300

    feature: proporções em medidas

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,30 @@
 import React from "react";
-import { StyleSheet } from "react-native";
 
-import { Container, Title } from "./styles";
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+} from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: "https://github.com/gbelther.png" }} />
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Gabriel</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
