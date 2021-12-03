2e2c20dcbee80779652c2dfd4eaadedb9d96ed2c
GBelther <belthergabriel@gmail.com>
2021-12-02 22:30:51 -0300

    feature: criação de estilos globais

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -5,17 +5,8 @@ import { Container, Title } from "./styles";
 
 export function Dashboard() {
   return (
-    <Container style={styles.container}>
+    <Container>
       <Title>Dashboard</Title>
     </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#999",
-  },
-});
