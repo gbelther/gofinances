5df84944a31a70323d4c1466114ae4b6c62a70a4
GBelther <belthergabriel@gmail.com>
2021-12-03 23:08:36 -0300

    feature: cartões de transações dinamicos

--- src/components/TransactionCard/styles.ts
+++ src/components/TransactionCard/styles.ts
@@ -2,11 +2,16 @@ import styled from "styled-components/native";
 import { Feather } from "@expo/vector-icons";
 import { RFValue } from "react-native-responsive-fontsize";
 
+interface TransctionProps {
+  type: "positive" | "negative";
+}
+
 export const Container = styled.View`
   background-color: ${({ theme }) => theme.colors.shape};
   border-radius: 5px;
 
   padding: 17px 24px;
+  margin-bottom: 16px;
 `;
 
 export const Title = styled.Text`
@@ -16,9 +21,12 @@ export const Title = styled.Text`
   margin-bottom: 16px;
 `;
 
-export const Amount = styled.Text`
+export const Amount = styled.Text<TransctionProps>`
   font-family: ${({ theme }) => theme.fonts.regular};
   font-size: ${RFValue(20)}px;
+  color: ${({ theme, type }) =>
+    type === "positive" ? theme.colors.success : theme.colors.attention};
+
   margin-top: 2px;
 `;
 
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { getBottomSpace } from "react-native-iphone-x-helper";
 
 import { HighlightCard } from "../../components/HighlightCard";
 import { TransactionCard } from "../../components/TransactionCard";
@@ -16,9 +17,43 @@ import {
   HighlightCards,
   Transactions,
   Title,
+  TransactionList,
 } from "./styles";
 
 export function Dashboard() {
+  const data = [
+    {
+      type: "positive",
+      title: "Desenvolvimento de site",
+      amount: "R$ 12.000,00",
+      category: {
+        name: "Vendas",
+        icon: "dollar-sign",
+      },
+      date: "13/04/2021",
+    },
+    {
+      type: "negative",
+      title: "Desenvolvimento de site",
+      amount: "R$ 59,00",
+      category: {
+        name: "Vendas",
+        icon: "coffee",
+      },
+      date: "13/04/2021",
+    },
+    {
+      type: "negative",
+      title: "Desenvolvimento de site",
+      amount: "R$ 1.200,00",
+      category: {
+        name: "Vendas",
+        icon: "shopping-bag",
+      },
+      date: "13/04/2021",
+    },
+  ];
+
   return (
     <Container>
       <Header>
@@ -58,8 +93,14 @@ export function Dashboard() {
 
       <Transactions>
         <Title>Listagem</Title>
-
-        <TransactionCard />
+        <TransactionList
+          data={data}
+          renderItem={({ item }) => <TransactionCard data={item} />}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{
+            paddingBottom: getBottomSpace(),
+          }}
+        />
       </Transactions>
     </Container>
   );
