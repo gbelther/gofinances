33c3127cf8a719a0d1caea327635ee96a8c68c60
GBelther <belthergabriel@gmail.com>
2021-12-10 23:00:39 -0300

    refactor: retirada de código não necessario

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -44,7 +44,7 @@ interface TransactionData {
 }
 
 export function Resume() {
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [totalByCategories, setTotalByCategories] = useState<CategoryData[]>(
     []
@@ -53,8 +53,6 @@ export function Resume() {
   const theme = useTheme();
 
   function handleDateChange(action: "next" | "prev") {
-    setIsLoading(true);
-
     if (action === "next") {
       setSelectedDate(addMonths(selectedDate, 1));
     } else {
@@ -63,6 +61,8 @@ export function Resume() {
   }
 
   async function loadData() {
+    setIsLoading(true);
+
     const dataKey = "@gofinances:transactions";
     const response = await AsyncStorage.getItem(dataKey);
     const responseFormatted = response ? JSON.parse(response) : [];
@@ -117,14 +117,10 @@ export function Resume() {
     setIsLoading(false);
   }
 
-  useEffect(() => {
-    loadData();
-  }, [selectedDate]);
-
   useFocusEffect(
     useCallback(() => {
       loadData();
-    }, [])
+    }, [selectedDate])
   );
 
   return (
