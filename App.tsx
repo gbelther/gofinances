dd90670e3e490dcea0ffe62c5e170064c897ea18
GBelther <belthergabriel@gmail.com>
2021-12-10 22:55:17 -0300

    feature: statusbar com cor clara

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { StatusBar } from "react-native";
 import AppLoading from "expo-app-loading";
 import { ThemeProvider } from "styled-components/native";
 
@@ -31,6 +32,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <NavigationContainer>
+        <StatusBar barStyle="light-content" />
         <AppRoutes />
       </NavigationContainer>
     </ThemeProvider>
